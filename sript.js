2d638fac36f23320a6a8a6ddb2a5ef1cb421e10e
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-02-23 22:39:51 +0100

    finalmente una risposta

--- sript.js
+++ sript.js
@@ -1,10 +1,13 @@
 
 
 
-let nomes = document.getElementById("nome");
+const ciccio = document.getElementById("nome");
+
 let prova = document.getElementById("nomeUtente");
 
+
 document.querySelector("button").addEventListener("click", function(){
-    prova.innerHTML= nomes;
+
+    prova.innerHTML=`<p> ${ciccio.value} </p>`
     
 });
