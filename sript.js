306ad8af32db9000330f156a260aa865c73a10a5
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-13 17:16:28 +0100

    modifiche

--- sript.js
+++ sript.js
@@ -1,13 +1,67 @@
 
 
+/* importato l'imput dell'utente dell'imput-text del html nel paragrafo con ID "nomeUtente"  */
+const inputNome = document.getElementById("nome");
+/* importo l'input dell'utente che dichiara i km da percorrere */
+let inputKm = document.getElementById("chilometri");
 
-const ciccio = document.getElementById("nome");
 
-let prova = document.getElementById("nomeUtente");
+/* inporto il contenitore dove ci stamperemo il nome utente */
+let nomeUtente = document.getElementById("nomeUtente");
+/* dove stamperemo l'offerta */
+let offerta = document.getElementById("offert");
+/* dove stamperemo la carrozza */
+let carrozza = document.getElementById("carrozza");
+/* dove stamperemo il codice CP */
+let codeCP = document.getElementById("codiceCP");
+/* importo l'input età per poi generare lo sconto se maggiorenne o minorenne */
+let età = document.getElementById("età")
+/* importo il costo del biglietto */
+let costoBiglietto = document.getElementById("costoBiglietto")
+/* genero un numero random da 1 a 20 che indicherà la carrozza */
+let randomNumber = Math.floor(Math.random() * 20 ) +1;
+/* genero un numero random per il codiceCP */
+let randomCode = Math.floor(Math.random() * 9999 ) +1;
+
+
+/* constante che dichiara il prezzo al km */
+const priceKm = 0.21;
+/* somma per calcolare il costo del biglietto in base ai km dell'utente e il costo per km */
+const somma = inputKm.value * 0.21;
+
+/* somma scontanta del 20% per i minorenni */
+const scontoSomma = (somma * 20) / 100;
+
+
+
 
 
 document.querySelector("button").addEventListener("click", function(){
 
-    prova.innerHTML=`<p> ${ciccio.value} </p>`
+    nomeUtente.innerHTML=`<p> ${inputNome.value} </p>`;
+
+    carrozza.innerHTML= randomNumber;
+    codeCP.innerHTML = randomCode;
+
+    if(età.value == "maggiorenne"){
+        offerta.innerHTML = "Offerta per persone maggiorenni"
+        costoBiglietto.innerHTML = somma;
+    }
+    else{
+        offerta.innerHTML = "Offerta per minorenni"
+        costoBiglietto.innerHTML = somma - scontoSomma;
+    }
+
+    
+  
+
+    
     
 });
+
+
+let km = document.getElementById("chilometri");
+
+
+
+
